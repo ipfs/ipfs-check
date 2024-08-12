7a286c2baef80db3e8f18a52d46da79d57157d47
Daniel Norman <1992255+2color@users.noreply.github.com>
2024-08-12 16:04:45 +0200

    Update daemon.go
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>

--- daemon.go
+++ daemon.go
@@ -190,7 +190,6 @@ func (d *daemon) runCheck(query url.Values) (*output, error) {
 	} else {
 		// If so is the data available over Bitswap?
 		out.DataAvailableOverBitswap = checkBitswapCID(ctx, testHost, c, ma)
-		// testHost.Network().Conns()
 		conns := testHost.Network().ConnsToPeer(ai.ID)
 		if len(conns) > 0 {
 			maddr := conns[0].RemoteMultiaddr()
